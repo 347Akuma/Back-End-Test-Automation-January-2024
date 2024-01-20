6f2f5c7690b2131ae9ca95ad2b052c212dc00ef2
Alexander Dimitrov <adimitrovx@gmail.com>
2024-01-21 00:34:42 +0200

    -

---
+++ 01. Back-End-Technologies-Basics/06. JavaScript-Fundamentals/11-person-info.js
@@ -0,0 +1,15 @@
+function personInfo(name1, name2, age){
+
+    let person = 
+    {
+        'firstName': name1,
+        'lastName': name2,
+        'age': age
+    }
+
+    return person;
+}
+personInfo("Peter", 
+"Pan",
+"20"
+);
