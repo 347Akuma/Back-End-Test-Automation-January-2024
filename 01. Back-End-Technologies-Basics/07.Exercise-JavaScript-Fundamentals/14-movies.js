8d12dd8b1dca993bd505c9a278351bc5f4f61052
Alexander Dimitrov <adimitrovx@gmail.com>
2024-01-23 09:27:47 +0200

    -

---
+++ 01. Back-End-Technologies-Basics/07.Exercise-JavaScript-Fundamentals/14-movies.js
@@ -0,0 +1,64 @@
+function solve(commands) {
+    'use strict';
+
+    const movies = [];
+    
+    for (const command of commands) {
+        if (command.includes('addMovie')) {
+            const movieName = command.replace('addMovie', '');
+            movies.push({
+                name: movieName,
+                date: '',
+                director: ''
+            })
+        }
+        else if (command.includes('directedBy')) {
+            const movieName = command.substr(0, command.indexOf('directedBy')).trim();
+            const directedBy = command.substr(command.indexOf('directedBy') + 'directedBy'.length).trim();
+
+            const result = movies.find(movie => movie.name === movieName);
+
+            if (!result) {
+                movies.push({
+                    name: movieName,
+                    director: directedBy,
+                    date: ''
+                })
+            }
+            else {
+                result.director = directedBy;
+            }
+        }
+        else if (command.includes('onDate')) {
+            const movieName = command.substr(0, command.indexOf('onDate')).trim();
+            const onDate = command.substr(command.indexOf('onDate') + 'onDate'.length).trim();
+
+            const result = movies.find(movie => movie.name === movieName);
+
+            if (!result) {
+                movies.push({
+                    name: movieName,
+                    date: onDate,
+                    director: ''
+                })
+            }
+            else {
+                result.date = onDate;
+            }
+        }
+    }
+
+    movies.filter(movie => movie.date && movie.director && movie.name)
+    .forEach(movie => console.log(JSON.stringify(movie)));
+}
+solve([
+    'addMovie Fast and Furious',
+    'addMovie Godfather',
+    'Inception directedBy Christopher Nolan',
+    'Godfather directedBy Francis Ford Coppola',
+    'Godfather onDate 29.07.2018',
+    'Fast and Furious onDate 30.07.2018',
+    'Batman onDate 01.08.2018',
+    'Fast and Furious directedBy Rob Cohen'
+    ]
+    );
